8a8750e80fa0ef18ebfe3469272e46041cc50818
Leevi Hokkanen <leevi@surrogate.tv>
2021-01-07 12:43:04 +0200

    led_test_game: Changed implementation to use pigpio instead of gpiozero. Added also comments to clarify what the code does.

--- games/led_test_game/led_test_game.py
+++ games/led_test_game/led_test_game.py
@@ -1,22 +1,40 @@
-from gpiozero import LED
+import logging
+
+import pigpio
+
 from surrortg import Game
 from surrortg.inputs import Switch
-import logging
+
+
+LED_PIN = 17
 
 
 class LEDSwitch(Switch):
     def __init__(self, score_cb):
-        self.led = LED(17)
+        # Connect to pigpio daemon
+        self._pi = pigpio.pi()
+        if not self._pi.connected:
+            raise RuntimeError("Could not connect to pigpio daemon.")
+
+        # Initialize LED pin
+        self._pi.set_mode(LED_PIN, pigpio.OUTPUT)
+        # Store reference to update score callback function
         self.score_cb = score_cb
 
     async def on(self, seat=0):
-        logging.info("LED on")
-        self.led.on()
+        self._pi.write(LED_PIN, pigpio.HIGH)
         self.score_cb()
+        logging.info("LED on")
 
     async def off(self, seat=0):
+        self._pi.write(LED_PIN, pigpio.LOW)
         logging.info("LED off")
-        self.led.off()
+
+    async def shutdown(self, seat):
+        # Set LED pin into safe state by setting it to input mode
+        self._pi.set_mode(LED_PIN, pigpio.INPUT)
+        # Close pigpio daemon connection
+        self._pi.close()
 
 
 class LedTestGame(Game):
@@ -24,11 +42,16 @@ class LedTestGame(Game):
         self.score += 1
         logging.info(f"score: {self.score}")
         if self.score >= 10:
+            # End game by sending final score to game engine.
             self.io.send_score(score=self.score, final_score=True)
+            # Disable all registered inputs.
+            self.io.disable_inputs()
         else:
+            # Send updated scores to game engine.
             self.io.send_score(score=self.score)
 
     async def on_init(self):
+        # Register LED input with update score callback function
         self.io.register_inputs({"switch": LEDSwitch(self.update_score)})
 
     async def on_prepare(self):
