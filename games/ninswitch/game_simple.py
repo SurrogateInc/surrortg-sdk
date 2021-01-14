c0cc222dfde9ea481b7319deecf0257ca22f3e63
Olli Paloviita <olli@surrogate.tv>
2021-01-14 16:39:48 +0200

    ninswitch: detection added for the home screen

--- games/ninswitch/game_simple.py
+++ games/ninswitch/game_simple.py
@@ -3,8 +3,34 @@ from games.ninswitch.ns_gamepad_serial import NSGamepadSerial, NSButton, NSDPad
 from games.ninswitch.ns_switch import NSSwitch
 from games.ninswitch.ns_dpad_switch import NSDPadSwitch
 from games.ninswitch.ns_joystick import NSJoystick
+from surrortg.image_recognition import AsyncVideoCapture, get_pixel_detector
+from pathlib import Path
+import logging
 import pigpio
 import asyncio
+import cv2
+
+
+# limit the processor use
+cv2.setNumThreads(1)
+
+# image rec
+SAVE_FRAMES = False
+SAVE_DIR_PATH = "/opt/srtg-python/imgs"
+
+# ((x, y), (r, g, b))
+HOME_CURRENT_GAME_SELECTED_PIXELS = [
+    ((22, 697), (52, 52, 52)),
+    ((25, 17), (52, 52, 52)),
+    ((1261, 16), (52, 52, 52)),
+    ((1258, 695), (52, 52, 52)),
+    ((288, 543), (252, 1, 16)),
+    ((344, 543), (255, 1, 12)),
+    ((164, 482), (17, 202, 255)),
+    ((173, 483), (4, 208, 255)),
+    ((169, 474), (7, 202, 255)),
+    ((427, 577), (87, 87, 87)),
+]
 
 
 class NinSwitchSimpleGame(Game):
@@ -52,6 +78,16 @@ class NinSwitchSimpleGame(Game):
             },
         )
 
+        # init image rec
+        self.is_home_current_selected = False
+        self.image_rec_task = asyncio.create_task(self.image_rec_main())
+        self.image_rec_task.add_done_callback(self.image_rec_done_cb)
+
+        # init frame saving
+        if SAVE_FRAMES:
+            logging.info(f"SAVING FRAMES TO {SAVE_DIR_PATH}")
+            Path(SAVE_DIR_PATH).mkdir(parents=True, exist_ok=True)
+
     """
     here you could do something with
     on_config, on_prepare, on_pre_game, on_countdown, on_start...
@@ -80,6 +116,46 @@ class NinSwitchSimpleGame(Game):
         # end controls
         self.nsg.end()
         self.pi.stop()
+
+        self.image_rec_task.cancel()
+
+    async def image_rec_main(self):
+        # create capture
+        self.cap = await AsyncVideoCapture.create("/dev/video21")
+
+        # get detector
+        has_home_current_game_selected = get_pixel_detector(
+            HOME_CURRENT_GAME_SELECTED_PIXELS
+        )
+
+        # loop through frames
+        i = 0
+        async for frame in self.cap.frames():
+            # detect
+            if has_home_current_game_selected(frame):
+                logging.info("is home, the current game selected")
+                self.is_home_current_selected = True
+            else:
+                self.is_home_current_selected = False
+
+            # generic
+            if i % 1000 == 0:
+                logging.info("1000 frames checked")
+            if SAVE_FRAMES:
+                cv2.imwrite(f"{SAVE_DIR_PATH}/{i}.jpg", frame)
+                logging.info(f"SAVED {i}.jpg")
+            i += 1
+
+    def image_rec_done_cb(self, fut):
+        # make program end if image_rec_task raises error
+        if not fut.cancelled() and fut.exception() is not None:
+            import traceback, sys  # noqa: E401
+
+            e = fut.exception()
+            logging.error(
+                "".join(traceback.format_exception(None, e, e.__traceback__))
+            )
+            sys.exit(1)
 
 
 if __name__ == "__main__":
