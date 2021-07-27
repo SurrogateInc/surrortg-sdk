42e32d8df0a0a971870824ba9997e2e6acf8fb23
Olli Paloviita <olli@surrogate.tv>
2021-07-27 14:11:48 +0300

    surrobot: fix for game crashing after reset if there was ongoing game

--- games/surrobot/game.py
+++ games/surrobot/game.py
@@ -1,10 +1,10 @@
-import asyncio
 import logging
 
-from surrortg import Game, ConfigType
-from surrortg.inputs import Joystick, LinearActuator, KeyCode
 from games.surrobot.hw import Hw
-from games.surrobot.template_games import Game1, RacingGame, Game3
+from games.surrobot.template_games import Game1, Game3, RacingGame
+from surrortg import ConfigType, Game
+from surrortg.inputs import Joystick, KeyCode, LinearActuator
+
 
 class MotorJoystick(Joystick):
     def __init__(self, motor_controller, custom_binds=None):
@@ -14,11 +14,12 @@ class MotorJoystick(Joystick):
     async def handle_coordinates(self, x, y, seat=0):
         self.motor_controller.rotational_speed = x
         self.motor_controller.longitudinal_speed = y
-    
+
     def get_default_keybinds(self):
         if self.custom_binds:
             return self.custom_binds
         return super().get_default_keybinds()
+
 
 class ServoJoystick(Joystick):
     def __init__(self, servo_x, servo_y=None, custom_binds=None):
@@ -30,12 +31,13 @@ class ServoJoystick(Joystick):
         self.servo_x.rotation_speed = x
         if self.servo_y:
             self.servo_y.rotation_speed = y
-    
+
     def get_default_keybinds(self):
         if self.custom_binds:
             return self.custom_binds
         return super().get_default_keybinds()
-    
+
+
 class ServoActuator(LinearActuator):
     def __init__(self, servo, custom_binds=None):
         self.servo = servo
@@ -43,24 +45,32 @@ class ServoActuator(LinearActuator):
 
     async def drive_actuator(self, val, seat=0):
         self.servo.rotation_speed = val
-    
+
     def get_default_keybinds(self):
         if self.custom_binds:
             return self.custom_binds
         return super().get_default_keybinds()
 
+
 class SurrobotGame(Game):
     async def on_init(self):
         # Game Type Enum (Racing-Game, Free-Roam, Tag)
         # Different game types can have restriction to slot options? & offer
-        # extra configuration spesific to game type. Every game-type has it's own game loop
-        # implementation. for e.g. racing-game requires 1 light sensor, 4 wheel or 2 wheel drive
+        # extra configuration spesific to game type.
+        # Every game-type has it's own game loop
+        # implementation. for e.g. racing-game requires 1 light sensor,
+        # 4 wheel or 2 wheel drive
         # And the implementation will track lap times and send them to GE
         self.io.register_config(
             "game-template", ConfigType.INTEGER, 1, False, minimum=1, maximum=3
         )
         self.io.register_config(
-            "game-template-2[conditional]-max-laps", ConfigType.INTEGER, 3, False, minimum=1, maximum=10
+            "game-template-2[conditional]-max-laps",
+            ConfigType.INTEGER,
+            3,
+            False,
+            minimum=1,
+            maximum=10,
         )
         # Movement slot (side 4x motors, 4x servos)
         # Offer 4 wheel drive, 2 wheel drive etc
@@ -68,7 +78,12 @@ class SurrobotGame(Game):
             "movement-slot", ConfigType.INTEGER, 1, False, minimum=1, maximum=4
         )
         self.io.register_config(
-            "movement-speed", ConfigType.INTEGER, 8, False, minimum=1, maximum=10
+            "movement-speed",
+            ConfigType.INTEGER,
+            8,
+            False,
+            minimum=1,
+            maximum=10,
         )
         # Top slot 1 (3x servos)
         # Offer servo arm, camera pivot, etc
@@ -84,59 +99,71 @@ class SurrobotGame(Game):
         self.inputs = {}
         self.hw = Hw()
         self.templates = [Game1(self.hw), RacingGame(self.hw), Game3(self.hw)]
-        
+
         # Preferably the input configs could be "live reloaded" based on the
         # "slot" & etc configuration. For e.g movement slot selection
         # "4 wheel drive" & "2 wheel drive" would give create following
         # joystick with correct motor_controller (4 or 2 wheel)
-        motor_joystick = MotorJoystick(self.hw.motor_controller, custom_binds=[
-            {
-                "up": KeyCode.KEY_W,
-                "down": KeyCode.KEY_S,
-                "left": KeyCode.KEY_A,
-                "right": KeyCode.KEY_D,
-            }
-        ])
+        motor_joystick = MotorJoystick(
+            self.hw.motor_controller,
+            custom_binds=[
+                {
+                    "up": KeyCode.KEY_W,
+                    "down": KeyCode.KEY_S,
+                    "left": KeyCode.KEY_A,
+                    "right": KeyCode.KEY_D,
+                }
+            ],
+        )
         self.inputs["movement"] = motor_joystick
 
         self.servos = self.hw.servos
 
-        camera = ServoJoystick(self.servos[0], self.servos[1], [
-            {
-                "up": KeyCode.KEY_ARROW_UP,
-                "down": KeyCode.KEY_ARROW_DOWN,
-                "left": KeyCode.KEY_ARROW_LEFT,
-                "right": KeyCode.KEY_ARROW_RIGHT,
-            }
-        ])
+        camera = ServoJoystick(
+            self.servos[0],
+            self.servos[1],
+            [
+                {
+                    "up": KeyCode.KEY_ARROW_UP,
+                    "down": KeyCode.KEY_ARROW_DOWN,
+                    "left": KeyCode.KEY_ARROW_LEFT,
+                    "right": KeyCode.KEY_ARROW_RIGHT,
+                }
+            ],
+        )
         self.inputs["top-slot-1-camera"] = camera
 
-        claw = ServoActuator(self.servos[2], [
-            {
-                "min": "KeyN",
-                "max": "KeyM",
-            }
-        ])
+        claw = ServoActuator(
+            self.servos[2],
+            [
+                {
+                    "min": "KeyN",
+                    "max": "KeyM",
+                }
+            ],
+        )
         self.inputs["bottom-slot-claw"] = claw
 
         self.io.register_inputs(self.inputs)
-        self.hw.reset_eyes()        
-    
+        self.hw.reset_eyes()
+
     async def on_config(self):
         # Read game template
         game_template = self.configs["custom"]["game-template"]
-        self.template = self.templates[game_template-1]
+        self.template = self.templates[game_template - 1]
         logging.info(f"game template: {game_template}")
         # Could we set inputs here based on the game template + slot config?
-    
+
     async def on_start(self):
         logging.info("Game starts")
         score = await self.template.on_start()
         self.io.send_score(score=score, final_score=True)
-    
+
     async def on_finish(self):
         logging.info("Game ends")
-        await self.template.on_finish()
+        # if reset during previous game self.template might not exist
+        if self.template:
+            await self.template.on_finish()
 
 
 if __name__ == "__main__":
