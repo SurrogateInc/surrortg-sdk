1c33baeec2adaf204a94b4e4f2593171256f2f17
Henri Dahl <henri@surrogate.tv>
2021-08-03 11:35:33 +0300

    aruco: add helper classes for easy aruco detection

---
+++ surrortg/image_recognition/aruco/aruco_filter.py
@@ -0,0 +1,149 @@
+import logging
+import time
+
+
+class ArucoFilter:
+    def __init__(
+        self,
+        callback,
+        aruco_source,
+        ids=[],
+        detect_distance=0,
+        detection_cooldown=0,
+    ):
+        """Helper class for filtering detected Aruco markers.
+
+        Use this helper class to receive callbacks only for aruco markers that
+        fulfill the filter criteria. Built-in filters exist for ID, distance,
+        and time interval. This class can be useful if you use aruco markers
+        for several types of game logic, or just to skip writing boilerplate
+        code.
+
+        Extra filters can also be added via the add_filter method.
+
+        Short example of how to integrate the class into game logic:
+        -----------------------------------------------------------------------
+        from surrortg.image_recognition.aruco import ArucoFilter
+
+        YourGame(Game):
+        async def on_init(self):
+            self.aruco_source = await ArucoDetect.create()
+            self.filter = ArucoFilter(self.score_logic, self.aruco_source,
+                [0,1,2,3,4], 0, 2)
+
+        async def on_start(self):
+            self.filter.start()
+
+        def score_logic(self, marker):
+            # implement logic for marker that passed filter
+            # if final_score:
+            #     self.filter.stop()
+        -----------------------------------------------------------------------
+
+        :param callback: Function to call if marker passes filters
+        :type callback: Function with one ArucoMarker as the only parameter
+        :param aruco_source: An ArucoDetect which the filter subscribes to
+            in order to receive detected aruco markers
+        :type aruco_source: ArucoDetect
+        :param ids: Aruco marker IDs which the filter will accept. If this is
+            an empty list, all IDs will be accepted.
+        :type ids: list of ints, optional
+        :param detect_distance: Distance threshold for accepting an aruco
+            marker. Must be between 0 and 1, as the distance is really the
+            relative size of the marker in the video frame. If zero, markers
+            at any distance are accepted.
+        :type detect_distance: float between 0 and 1, optional
+        :param detection_cooldown: If set above zero, a marker can only be
+            detected once within the specified cooldown time.
+        :type detection_cooldown: number, optional
+        """
+        self.callback = callback
+        self.ids = ids
+        self.min_dist = detect_distance
+        self.aruco_source = aruco_source
+        self.det_cd = detection_cooldown
+        self.det_times = {}
+        self.running = False
+        self.filters = [self._filter_by_id, self._filter_by_distance]
+        if detection_cooldown > 0:
+            self.filters.append(self._filter_by_time)
+
+    def add_filter(self, filter_fun):
+        """Add custom filter to the ArucoFilter instance.
+
+        :param filter_fun: Custom filter function
+        :type filter_fun: Function with one ArucoMarker as the only parameter
+            and truthy return value (i.e. bool)
+        """
+        self.filters.append(filter_fun)
+
+    def remove_filter(self, filter_fun):
+        """Remove custom filter from the ArucoFilter instance.
+
+        :param filter_fun: Custom filter function
+        :type filter_fun: Function with one ArucoMarker as the only parameter
+            and truthy return value (i.e. bool)
+        """
+        try:
+            self.filters.remove(filter_fun)
+        except ValueError as e:
+            logging.warning(
+                f"Warning: error in removing filter from aruco filters: {e}"
+            )
+            pass
+
+    def start(self):
+        """Make the ArucoFilter subscribe for new aruco marker detections.
+        Markers which pass the filter will then be sent via the callback
+        given at __init__.
+        """
+        if not self.running:
+            self.aruco_source.register_observer(self._detect_cb)
+            self.running = True
+
+    def stop(self):
+        """Make the ArucoFilter unsubscribe from new aruco marker detections.
+        No more markers will be sent via the callback, unless start() is
+        called again.
+        """
+        if self.running:
+            self.aruco_source.unregister_observer(self._detect_cb)
+            self.running = False
+
+    def set_cooldown(self, cooldown):
+        """Set cooldown timer for marker detection.
+
+        :param cooldown: Cooldown time. If zero, no cooldown is used. Otherwise
+             a marker will only be detected once every cooldown period.
+        :type cooldown: nonnegative number
+        """
+        if cooldown == 0:
+            self.remove_filter(self._filter_by_time)
+            self.det_cd = cooldown
+            return
+        if self.det_cd == 0:
+            self.filters.append(self._filter_by_time)
+        self.det_cd = cooldown
+
+    def _detect_cb(self, found_markers):
+        for marker in found_markers:
+            if self._filter_marker(marker):
+                self.callback(marker)
+
+    def _filter_by_id(self, marker):
+        return len(self.ids) == 0 or marker.id in self.ids
+
+    def _filter_by_distance(self, marker):
+        return self.min_dist == 0 or marker.get_distance() > self.min_dist
+
+    def _filter_by_time(self, marker):
+        if marker.id not in self.det_times:
+            self.det_times[marker.id] = time.time()
+            return True
+        if time.time() - self.det_times[marker.id] < self.det_cd:
+            return False
+        self.det_times[marker.id] = time.time()
+        return True
+
+    def _filter_marker(self, marker):
+        return all([f(marker) for f in self.filters])
---
+++ surrortg/image_recognition/aruco/aruco_finder.py
@@ -0,0 +1,163 @@
+import logging
+
+from surrortg.game_io import ConfigType
+from surrortg.image_recognition.aruco import ArucoDetect, ArucoFilter
+
+DEFAULT_CAMERA = "/dev/video21"
+
+CUSTOM_KEY = "custom"
+NUM_MARKERS_KEY = "Number of aruco markers to find"
+MIN_DISTANCE_KEY = "Minimum distance for detection (0 for any distance)"
+IN_ORDER_KEY = "Markers must be detected in order"
+IN_ORDER_DEF_VAL = False
+NUM_LAPS_KEY = "Number of laps"
+
+
+class ArucoFinder:
+    @classmethod
+    async def create(
+        cls,
+        io,
+        source=DEFAULT_CAMERA,
+        num_markers=5,
+        min_distance=100,
+        in_order=False,
+        num_laps=1,
+        bot_specific=False,
+        seat=0,
+    ):
+        """Class for creating treasure hunt and racing games with aruco markers
+
+        This class is an easy way to incorporate aruco markers into a game. The
+        class creates its own ArucoDetect instance and has built-in game
+        logic, so only a few lines of code are required to use the class.
+
+        The class sends configuration options to the dashboard Game Engine
+        settings, where the configuration parameters can be changed. For a
+        racing type game, set in_order to True.
+
+        Short example of how to integrate the class into game logic:
+        -----------------------------------------------------------------------
+        from surrortg.image_recognition.aruco import ArucoFinder
+
+        YourGame(Game):
+        async def on_init(self):
+            self.finder = await ArucoFinder.create(self.io)
+
+        async def on_config(self):
+            self.finder.on_config(self.configs)
+
+        async def on_start(self):
+            self.finder.on_start()
+        -----------------------------------------------------------------------
+
+        :param io: GameIO instance, used to register configs and send scores
+            to the game engine
+        :type io: GameIO
+        :param source: Video capture device used for detecting aruco markers
+        :type source: string, optional
+        :param num_markers: number of aruco IDs to use. IDs will be generated
+            from 0 to num_markers-1
+        :type num_markers: int
+        :param min_distance: Distance threshold for detecting an aruco
+            marker. This class uses an arbitrary unit of distance between 0 and
+            500
+        :type min_distance: float between 0 and 500, optional
+        :param in_order: Whether markers must be detected in ascending
+            numerical order. Useful for racing games
+        :type in_order: bool, optional
+        :param num_laps: Number of laps in one game
+        :type num_laps: int, optional
+        :param bot_specific: Whether the configuration options in the web
+            interface are in effect for all bots, or just this bot
+        :type bot_specific: bool, optional
+        :param seat: Seat number of the bot
+        :type seat: int, optional
+        """
+        self = cls()
+        self.io = io
+        self.seat = seat
+        self.io.register_config(
+            NUM_MARKERS_KEY,
+            ConfigType.INTEGER,
+            num_markers,
+            bot_specific,
+            minimum=1,
+            maximum=50,
+        )
+        self.io.register_config(
+            MIN_DISTANCE_KEY,
+            ConfigType.INTEGER,
+            min_distance,
+            bot_specific,
+            minimum=0,
+            maximum=500,
+        )
+        self.io.register_config(
+            NUM_LAPS_KEY,
+            ConfigType.INTEGER,
+            num_laps,
+            bot_specific,
+            minimum=1,
+            maximum=100,
+        )
+        self.io.register_config(
+            IN_ORDER_KEY, ConfigType.BOOLEAN, in_order, bot_specific
+        )
+        self.aruco_source = await ArucoDetect.create(source)
+        self.filter = ArucoFilter(
+            self._score_logic,
+            self.aruco_source,
+            self._gen_id_list(num_markers),
+            min_distance,
+        )
+        return self
+
+    def on_config(self, configs):
+        """Parses the configs object for configs from web interface. MUST BE
+            CALLED every time at the on_config part of the game loop.
+
+        :param configs: self.configs from the Game object which owns this
+            instance
+        :type configs: dict
+        """
+        self.filter.stop()
+        self.num_markers = configs[CUSTOM_KEY][NUM_MARKERS_KEY]
+        if configs[CUSTOM_KEY][MIN_DISTANCE_KEY] == 0:
+            self.filter.min_dist = 0
+        else:
+            self.filter.min_dist = 0.5 / configs[CUSTOM_KEY][MIN_DISTANCE_KEY]
+        self.num_laps = configs[CUSTOM_KEY][NUM_LAPS_KEY]
+        self.in_order = configs[CUSTOM_KEY][IN_ORDER_KEY]
+        self.filter.ids = self._gen_id_list(self.num_markers)
+        logging.info(
+            f"ArucoFinder configured with markers: {self.filter.ids}"
+            f" and minimum marker size: {self.filter.min_dist}"
+        )
+
+    def on_start(self):
+        """Starts the aruco detection and begins counting scores. MUST BE
+        CALLED every time at the on_start part of the game loop.
+        """
+        self.score = 0
+        self.cur_lap = 1
+        self.filter.start()
+
+    def _score_logic(self, marker):
+        is_final = False
+        if not self.in_order or (
+            self.in_order and marker.id == min(self.filter.ids)
+        ):
+            self.filter.ids.remove(marker.id)
+            self.score += 1
+        if len(self.filter.ids) == 0:
+            if self.cur_lap == self.num_laps:
+                self.filter.stop()
+                is_final = True
+            else:
+                self.cur_lap += 1
+                self.filter.ids = self._gen_id_list(self.num_markers)
+        self.io.send_score(self.score, seat=self.seat, final_score=is_final)
+
+    def _gen_id_list(self, num_ids):
+        return set(marker_id for marker_id in range(num_ids))
