345336c97121d0ca896693b6d1b7b2ff363abb13
Sakari Tanskanen <sakari@surrogate.tv>
2021-08-10 13:14:57 +0300

    configs: new custom config format
    Add custom configs using a dict-based interface.
    
    The new interface makes defining complicated configurations simpler.
    It also adds a few new features to the config system.
    
    - Titles can be defined for configs. This allows a decoupling between
      the id which is used in code and the displayed title of the config
    - Descriptions can be added to configs
    - Individual enum values can have descriptions attached to them
    - Groups can be nested
    - Configs can have multiple conditions and groups can have conditions
    
    In addition, the old config interface is deprecated but continues to
    work until further notice

--- surrortg/game_io.py
+++ surrortg/game_io.py
@@ -15,6 +15,151 @@ class ConfigType(Enum):
     BOOLEAN = "boolean"
 
 
+"""
+EXAMPLE_CONF = {
+    "children": {
+        "myconf": {
+            "title": "My conf displayname",
+            "description": "Describes the conf in detail",
+            "valueType": ConfigType.NUMBER,
+            "default": 1,
+            "enum": [
+                { "value": 1, "description": "description when selected" },
+                { "value": 2, "description": "another description" },
+            ],
+        },
+        "condconf": {
+            "title": "Conditional config",
+            "valueType": ConfigType.BOOLEAN,
+            "default": False,
+            "conditions": [
+                { "variable": "myconf", "value": 1 },
+                { "variable": "mygroup.mysubconf", "value": 3 },
+            ],
+        },
+        "mygroup": {
+            "conditions": [
+                { "variable": "myconf", "value": 1 },
+            ],
+            "children": {
+                "mysubconf": {
+                    "title": "My Sub Conf",
+                    "valueType": ConfigType.INTEGER,
+                    "default": 3,
+                    "minimum": 2,
+                    "maximum": 50,
+                },
+                "myothersubconf": {
+                    "valueType": ConfigType.BOOLEAN,
+                    "default": False,
+                },
+            },
+        },
+    }
+}
+"""
+
+EMPTY_CONFIG = {"children": {}}
+
+
+def check_conditions(config, parent, root):
+    if "conditions" not in config:
+        return
+    for condition in config["conditions"]:
+        assert isinstance(condition["variable"], str)
+        is_relative = condition["variable"][0] == "."
+        real_root = parent if is_relative else root
+        try:
+            for part in filter(None, condition["variable"].split(".")):
+                real_root = real_root["children"][part]
+        except KeyError:
+            var = condition["variable"]
+            raise AssertionError(f"Condition {var} not found")
+        print(real_root)
+        value_type = real_root["valueType"]
+        if type(value_type) is not str:
+            value_type = value_type.value
+        types = _get_config_types(value_type)
+        assert isinstance(condition["value"], types)
+
+
+def check_title(id, config):
+    assert "title" not in config or isinstance(
+        config["title"], str
+    ), f"[{id}] Display name must be a string"
+
+
+def check_config_group(id, group, root):
+    check_title(id, group)
+    for id, config in group["children"].items():
+        check_config_entry(id, config, group, root)
+
+
+def check_config_variable(id, variable, root):
+    check_title(id, variable)
+    assert "description" not in variable or isinstance(
+        variable["description"], str
+    ), f"[{id}] Description must be a string"
+    value_type = variable["valueType"]
+    assert isinstance(value_type, ConfigType), (
+        f"[{id}] 'value_type' must be a valid ConfigType ",
+        "(string | number | integer | bool)",
+    )
+    if type(value_type) is not str:
+        value_type = value_type.value
+        variable["valueType"] = value_type
+    default = variable["default"]
+    maximum = variable["maximum"] if "maximum" in variable else None
+    minimum = variable["minimum"] if "minimum" in variable else None
+    enum = variable["enum"] if "enum" in variable else None
+    types_to_check = _get_config_types(value_type)
+    assert isinstance(
+        default, types_to_check
+    ), f"'default' must be a {value_type}"
+
+    minmax_values = tuple(set(types_to_check).intersection(set([int, float])))
+    for val in [minimum, maximum]:
+        assert val is None or isinstance(val, minmax_values)
+        f"[{id}] min/max val has to be float, int or None"
+
+    if enum is not None:
+        assert (
+            isinstance(enum, list) and len(enum) > 0
+        ), "Enum must be a non-empty list"
+        for val in enum:
+            assert isinstance(
+                val["value"], types_to_check
+            ), f"enum values must be of type {value_type}"
+            assert val["description"] is None or isinstance(
+                val["description"], str
+            ), "Enum description must be a string"
+        assert default in map(
+            lambda x: x["value"], enum
+        ), "Default value must be one of enum values"
+
+    if minimum is not None:
+        assert default >= minimum, "'default' must be at least minimum"
+    if maximum is not None:
+        assert default <= maximum, "'default' must be at most maximum"
+
+    if enum is not None:
+        assert (
+            minimum is None and maximum is None
+        ), "enum and min/max are mutually exclusive"
+
+
+def check_config_entry(id, config, parent, root):
+    check_conditions(config, parent, root)
+    if "children" in config:
+        check_config_group(id, config, root)
+    else:
+        check_config_variable(id, config, root)
+
+
+def check_configs(config):
+    check_config_group("root", config, config)
+
+
 def _get_config_types(value_type):
     types_to_check = ()
     if value_type == "string":
@@ -50,6 +195,8 @@ class GameIO:
         self.device_id = self._config["device_id"]
         self.input_bindings = {}
         self._custom_configs = []
+        self._robot_configs = EMPTY_CONFIG
+        self._global_configs = EMPTY_CONFIG
         self._message_router = MultiSeatMessageRouter(robot_log_handler)
 
         # If type is not string (deprecated old interface), assume type is
@@ -147,19 +294,6 @@ class GameIO:
             is_robot_specific, bool
         ), "'is_robot_specific' must be a boolean"
         types_to_check = _get_config_types(value_type)
-        if value_type == "string" or value_type == "boolean":
-            assert (
-                minimum is None and maximum is None
-            ), "String config cannot have min/max value"
-        elif value_type == "number":
-            for val in [minimum, maximum]:
-                assert val is None or isinstance(
-                    val, (int, float)
-                ), "min/max val has to be float, int or None"
-        elif value_type == "integer":
-            for val in [minimum, maximum]:
-                assert val is None or isinstance(val, int)
-                "min/max val has to be float, int or None"
 
         assert isinstance(
             default, types_to_check
@@ -236,6 +370,14 @@ class GameIO:
             }
 
         self._custom_configs.append(obj)
+
+    def set_robot_configs(self, configs):
+        check_config_group(configs, configs, configs)
+        self._robot_configs = configs
+
+    def set_global_configs(self, configs):
+        check_config_group(configs, configs, configs)
+        self._global_configs = configs
 
     def register_inputs(self, inputs, admin=False, bindable=True):
         """Registers inputs
@@ -525,7 +667,14 @@ class GameIO:
             logging.info(message)
 
     def _send_controller_ready(self):
-        msg = {"inputs": self._get_inputs(), "configs": self._custom_configs}
+        if len(self._custom_configs) > 0:
+            configs = self._custom_configs
+        else:
+            configs = {
+                "robotConfigs": self._robot_configs,
+                "globalConfigs": self._global_configs,
+            }
+        msg = {"inputs": self._get_inputs(), "configs": configs}
         logging.info(f"Sending controller ready: {msg}")
         self._send_threadsafe("controllerReady", payload=msg)
 
