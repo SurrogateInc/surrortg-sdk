77f3cde62e4c3369e8f4da1243928abfa04e6af1
Henri Dahl <henri@surrogate.tv>
2021-08-30 13:18:52 +0300

    network: add custom exception for GE connection issues in ge_api_client

--- surrortg/__init__.py
+++ surrortg/__init__.py
@@ -2,4 +2,4 @@ from .config_parser import get_config
 from .custom_config import ConfigType
 from .game import Game, RobotType
 from .game_io import GameIO, ScoreType, SortOrder
-from .network.ge_api_client import ApiClient
+from .network.ge_api_client import ApiClient, GEConnectionError
