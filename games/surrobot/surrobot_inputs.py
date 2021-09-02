b35f8b3da956927240720adeb24171e65c515573
Esko Vähämäki <esko@surrogate.tv>
2021-09-02 16:45:14 +0300

    surrobot: default mobine inputs
    WHY:
    
    - V1 bot needs to support mobile inputs out of the box
    
    WHAT:
    
    - Implemented new defaults for the current surrobot inputs
    - Added names for inputs

--- games/surrobot/surrobot_inputs.py
+++ games/surrobot/surrobot_inputs.py
@@ -1,72 +1,57 @@
 from games.surrobot.surrobot_config import Extension, Slot
-from surrortg.inputs import Joystick, KeyCode, LinearActuator, Switch
+from surrortg.inputs import (
+    Joystick,
+    KeyCode,
+    LinearActuator,
+    Switch,
+    keys_object,
+    on_screen_position,
+)
 
 
 class MotorJoystick(Joystick):
-    def __init__(self, motor_controller, custom_binds=None):
+    def __init__(self, motor_controller, defaults=None):
+        super().__init__(defaults)
         self.motor_controller = motor_controller
-        self.custom_binds = custom_binds
 
     async def handle_coordinates(self, x, y, seat=0):
         self.motor_controller.rotational_speed = x
         self.motor_controller.longitudinal_speed = y
 
-    def get_default_keybinds(self):
-        if self.custom_binds:
-            return self.custom_binds
-        return super().get_default_keybinds()
-
 
 class ServoJoystick(Joystick):
-    def __init__(self, servo_x, servo_y=None, custom_binds=None):
+    def __init__(self, servo_x, servo_y=None, defaults=None):
+        super().__init__(defaults)
         self.servo_x = servo_x
         self.servo_y = servo_y
-        self.custom_binds = custom_binds
 
     async def handle_coordinates(self, x, y, seat=0):
         self.servo_x.rotation_speed = x
         if self.servo_y:
             self.servo_y.rotation_speed = y
 
-    def get_default_keybinds(self):
-        if self.custom_binds:
-            return self.custom_binds
-        return super().get_default_keybinds()
-
 
 class ServoActuator(LinearActuator):
-    def __init__(self, servo, custom_binds=None):
+    def __init__(self, servo, defaults=None):
+        super().__init__(defaults)
         self.servo = servo
-        self.custom_binds = custom_binds
 
     async def drive_actuator(self, val, seat=0):
         self.servo.rotation_speed = val
 
-    def get_default_keybinds(self):
-        if self.custom_binds:
-            return self.custom_binds
-        return super().get_default_keybinds()
-
 
 class ServoButton(Switch):
-    def __init__(
-        self, servo, on_position=1, off_position=-1, custom_binds=None
-    ):
+    def __init__(self, servo, on_position=1, off_position=-1, defaults=None):
+        super().__init__(defaults)
         self.servo = servo
         self.on_position = on_position
         self.off_position = off_position
-        self.custom_binds = custom_binds
 
     async def off(self, seat=0):
         await self.servo.rotate_to(self.off_position)
 
     async def on(self, seat=0):
         await self.servo.rotate_to(self.on_position)
-
-    def get_default_keybinds(self):
-        if self.custom_binds:
-            return self.custom_binds
-        return super().get_default_keybinds()
 
 
 def generate_inputs(hw, config_parser):
@@ -77,14 +62,14 @@ def generate_inputs(hw, config_parser):
         # TODO: Set the motor_controller to some 4 or 2 wheel mode
         motor_joystick = MotorJoystick(
             hw.motor_controller,
-            custom_binds=[
-                {
-                    "up": KeyCode.KEY_W,
-                    "down": KeyCode.KEY_S,
-                    "left": KeyCode.KEY_A,
-                    "right": KeyCode.KEY_D,
-                }
-            ],
+            defaults={
+                "humanReadableName": "movement",
+                "onScreenPosition": on_screen_position(20, 80, 20),
+                "xMinKeys": keys_object("left", [KeyCode.KEY_A]),
+                "xMaxKeys": keys_object("right", [KeyCode.KEY_D]),
+                "yMinKeys": keys_object("back", [KeyCode.KEY_S]),
+                "yMaxKeys": keys_object("forward", [KeyCode.KEY_W]),
+            },
         )
         inputs["movement"] = motor_joystick
 
@@ -93,14 +78,14 @@ def generate_inputs(hw, config_parser):
         camera = ServoJoystick(
             hw.servos[0],
             hw.servos[1],
-            [
-                {
-                    "up": KeyCode.KEY_ARROW_UP,
-                    "down": KeyCode.KEY_ARROW_DOWN,
-                    "left": KeyCode.KEY_ARROW_LEFT,
-                    "right": KeyCode.KEY_ARROW_RIGHT,
-                }
-            ],
+            defaults={
+                "humanReadableName": "look",
+                "onScreenPosition": on_screen_position(80, 80, 20),
+                "xMinKeys": keys_object("left", [KeyCode.KEY_ARROW_LEFT]),
+                "xMaxKeys": keys_object("right", [KeyCode.KEY_ARROW_RIGHT]),
+                "yMinKeys": keys_object("down", [KeyCode.KEY_ARROW_DOWN]),
+                "yMaxKeys": keys_object("up", [KeyCode.KEY_ARROW_UP]),
+            },
         )
         inputs["topFrontCamera"] = camera
 
@@ -108,16 +93,23 @@ def generate_inputs(hw, config_parser):
     if bottom_front_slot is Extension.CLAW:
         claw = ServoActuator(
             hw.servos[4],
-            [
-                {
-                    "min": "KeyN",
-                    "max": "KeyM",
-                }
-            ],
+            defaults={
+                "humanReadableName": "claw",
+                "onScreenPosition": on_screen_position(50, 80, 20),
+                "minKeys": keys_object("close", [KeyCode.KEY_N]),
+                "maxKeys": keys_object("open", [KeyCode.KEY_M]),
+            },
         )
         inputs["bottomFrontClaw"] = claw
     elif bottom_front_slot is Extension.BUTTON_PRESSER:
-        presser = ServoButton(hw.servos[4], custom_binds=["KeyN"])
+        presser = ServoButton(
+            hw.servos[4],
+            defaults={
+                "humanReadableName": "button",
+                "onScreenPosition": on_screen_position(50, 80, 20),
+                "keys": [KeyCode.KEY_N],
+            },
+        )
         inputs["bottomFrontButtonPresser"] = presser
 
     return inputs
